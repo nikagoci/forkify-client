0a5bfd382c865cd4cb6e2a97c8ca09558635e8c5
nikagoci <dato.svanidze2005@mail.ru>
2023-01-15 11:55:48 +0400

    change loading state

--- src/components/ResultSide/ResultSide.js
+++ src/components/ResultSide/ResultSide.js
@@ -35,13 +35,13 @@ const ResultSide = ({ setRecipeId }) => {
       if (ctx.query) {
         const allResponse = await axios.get(
           `https://fullforkify-api.onrender.com/api/v1/recipes?&recipe=${ctx.query}`
-        )
+        );
 
         setAllRecipe(allResponse.data.length);
       }
     }
-    fetchData()
-  }, [ctx.query])
+    fetchData();
+  }, [ctx.query]);
 
   useEffect(() => {
     if (
@@ -66,23 +66,35 @@ const ResultSide = ({ setRecipeId }) => {
         flexBasis="30%"
         backgroundColor="#fff"
         padding="40px 0"
-        height='820px'
+        height="820px"
         alignItems="center"
       >
-        <Typography variant="h5" component='h4' color='#f38e82' marginBottom={4}>Please Search Valid Recipe</Typography>
-        <Typography variant="h6" component='h5' color='#ff9319'>Current Recipes: Pizza, Burger, Salad</Typography>
+        <Typography
+          variant="h5"
+          component="h4"
+          color="#f38e82"
+          marginBottom={4}
+        >
+          Please Search Valid Recipe
+        </Typography>
+        <Typography variant="h6" component="h5" color="#ff9319">
+          Current Recipes: Pizza, Burger, Salad
+        </Typography>
+      </Stack>
+    );
+  }
+
+  if (loading && ctx.query) {
+    return (
+      <Stack flexBasis="30%" backgroundColor="#fff" padding="40px 0" height='820px'>
+        <Spinner />
       </Stack>
     );
   }
 
   return (
     <>
-      <Stack
-        flexBasis="30%"
-        backgroundColor="#fff"
-        padding="40px 0"
-      >
-        {loading && ctx.query && <Spinner />}
+      <Stack flexBasis="30%" backgroundColor="#fff" padding="40px 0">
         <Stack marginBottom="15px">
           {data.map((recipe, ind) => (
             <SingleResult
